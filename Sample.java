5130ff347cdcc0cf50a73e6e33d91ab6f8a2d171
Sivasurya002 <ndsivasurya002@gmail.com>
2023-11-03 16:27:37 +0530

    Chat & post

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
 public static void main(String[] args){
 System.out.println("Whatsapp developed");
 System.out.println("You can chat with ur family");
+System.out.println("You can send photos oin chat");
 }
 }
--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test{
 public static void main(String[] args){
 System.out.println("Instagram developed");
 System.out.println("You can upload photo as post");
+System.out.println("You can upload 30sec video as post");
 }
 }
